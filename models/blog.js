2ab7d34eff741b2429169e873da5d9eba74af7f9
loredana-popa <loredana.iacob87@gmail.com>
2022-11-21 20:53:43 +0200

    4.2 Blog list, step2

--- models/blog.js
+++ models/blog.js
@@ -1,24 +1,10 @@
 const mongoose = require('mongoose')
 
-const mongoUrl = process.env.MONGODB_URI
-const cld = `mongodb+srv://loredana:7ROaFPUT0qsWPf1a@cluster0.oa1cb03.mongodb.net/blogApp?retryWrites=true&w=majority`
-
-console.log('connecting to', mongoUrl)
-
-mongoose
-    .connect(mongoUrl)
-    .then((result) => {
-        console.log('connected')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
 const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
     url: String,
     likes: Number
-  })
+})
   
-  module.exports = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model('Blog', blogSchema)
