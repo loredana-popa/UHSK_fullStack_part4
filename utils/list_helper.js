a808d7557c167bbe45b1272c892692f27523bba3
loredana-popa <loredana.iacob87@gmail.com>
2022-11-23 14:57:32 +0200

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
     return 1
 }
   
+const totalLikes = (blogs) => {
+    const likesArr = blogs.map(blog => {
+        return blog.likes
+    })
+
+    const reducer = (sum, item) =>{
+        return sum + item
+    }
+    
+    return likesArr.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
