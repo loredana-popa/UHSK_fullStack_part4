c86ac2c063e5b38dc34156623d87e9f4ed515ff2
loredana-popa <loredana.iacob87@gmail.com>
2022-12-05 17:43:21 +0200

    4.19: bloglist expansion, step7

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const { default: mongoose } = require('mongoose')
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -7,12 +8,24 @@ blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1} )
     response.json(blogs)
 })
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.post('/',  async (request, response) => {
     const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
 
-    const user = await User.findOne({ id:'38a01919edbe77758e3aef5'})
-    console.log('user is', user)
+    if (!decodedToken) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
     
     const blog = new Blog({
         title: body.title,
